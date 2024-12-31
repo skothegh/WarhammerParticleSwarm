22b701932b9c23ddcc9041a9c56bf59107f027ad
skothegh <73349739+skothegh@users.noreply.github.com>
2024-12-31 15:05:58 +0000

    Add files via upload
    Initial Commit

---
+++ readme.txt
@@ -0,0 +1,188 @@
+1. Description
+
+Three things I greatly enjoy are numerical optimization, data visualisation
+and Warhammer 40k (40k). In 40k players build armies using miniatures according to
+some point costs and limits. An idea I had for a while was to see if one could use
+Monte Carlo to simulate fights and particle swarm optimization to adjust points.
+
+The code picks a number of opposing models corresponding to their point costs and
+the lowest common multiple of these costs, i.e. if a guardsman costs 5p and a
+space marine costs 20, four guardsmen will be picked and one space marine.
+
+Models in 40k have various attributes which make it more or less likely for them
+to wound an enemy model or be wounded in turn. A typical "ranged combat" involves
+A. rolling to hit,
+B. rolling to wound
+C. rolling armour saves,
+D. applying damage.
+Steps A.-C. are highly random in nature and will be simulated using Monte Carlo.
+
+Over the course of a several such fights it should become clear if one side is
+stronger than the other, which gives an indicator whether it is over- or under-
+costed, as well as by how much. This can then be used to calculate the cost as the
+the squared distance from the optimum:
+|Nwins / Nfights - 0.5|^2
+
+2. Classes
+class DataSheet(): contains the characteristics of a given model
+    def __init__(self,*,Name, Wounds, Toughness, Save, Invuln, Weapons):
+        self.name = Name
+        self.w = Wounds
+        self.sv = Save
+        self.inv = Invuln
+        self.t = Toughness
+        self.weapon = Weapons
+
+class Weapon(): contains the characteristics of the weapons
+    def __init__(self,*, Attacks, Skill, Strength, Damage, ArmourPiercing):
+        self.at = Attacks
+        self.sk = Skill
+        self.st = Strength
+        self.d = Damage
+        self.ap = ArmourPiercing
+
+class unit(): used during monte carlo to collect datasheet and model count into one structure
+    def __init__(self, ID, datasheet, number):
+        self.id = ID
+        self.model = datasheet
+        self.number = number
+
+
+3. Functions
+3.1. Monte Carlo
+- RollD6(n): returns n random numbers between 1 and 6.
+
+- Rollhits(model, count): returns number of successful hit rolls given the count
+of the model and the weapon it is equipped with.
+
+- WoundRoll(strength, toughness, count): returns number of successful wound roll
+given the weapons strength and the targets toughness
+
+- FailedSaveRolls(save,invuln,count): returns the number of failed saving throws
+given the targets armour characteristic and the count of successful wound rolls.
+
+- HandleDamage(wounds,failed_saves,damage): returns the number of destroyed models
+given the targets number of wounds, the number of failed saves, and the weapon damage.
+
+- CombatRound(attacker, defender): runs above functions in order.
+returns attacker, defender with reduced numbers.
+
+- SimulateRangedCombat(number_of_combats, model1, model2, points): runs CombatRound()
+for as long as at least one model is alive. returns number of wins for each model
+
+- DegreeOfTrust(data, p): returns bayesian degree of belief that Hypothesis that
+"Point estimates are accurate such that model1 and model2 are well balanced" is true
+
+3.2 Optimization
+- BurnIn(burn_in_samples, number_of_particles, queue, points, cost, total_cost):
+initialises cost matrix by letting each model fight each other model for each
+particle in the swarm
+
+- OptimizationStep(nparticle, index,  number_of_samples, points, queue, cost):
+For each particle, for chosen model (index) fight against each other model. Update
+and return cost matrix
+
+- Epoch(number_of_samples, number_of_particles, queue, points, cost, total_cost, best):
+If particle is locally best, pick largest contributor to its cost and minimize it
+one step. Else, find globally best particle and move step in its direction along
+one axis.
+
+- Driver(number_of_epochs,number_burnin_samples,number_of_samples,number_of_particles,queue,lims):
+initialises cost matrix, runs the optimization procedure.
+returns cost, total cost, cost_history, point_history
+
+
+4. Optimization procedure
+The procedure I use is an adapted particle swarm optimizer. I chose PSO as there
+is no explicit cost function that can be optimized and a meta-heuristic was necessary.
+Standard PSO uses a number of particles which move in search-space according to some
+innate velocity plus a velocity determined by a local and global best known position.
+
+In the case of the present code the search space is the point costs of the models.
+A particle is hence the cost-matrix evaluated at the given coordinates in point-space.
+This matrix contains the win-ratios of each model against each other model. The
+total_cost is estimated by the average win-ration of a model against each other model
+using Monte Carlo.
+
+This estimation is 1. very costly and 2. highly co-dependent, i.e.
+increasing the win-rate of one model causes the win rate of all other models to be
+decreased. Hence, I decided to only ever update a single model's point costs. This
+allows optimization to be performed somewhat efficient while being as careful as
+possible.
+
+The direction is chosen in either of two ways: If the particle is the best among its
+neighbours, it will pick the model which has the highest contribution to the total cost,
+i.e. is the most unbalanced, and try to carefully improve its points.
+If the particle is not locally the best, it will move in the direction of the best
+known position in search-space.
+
+This has the following advantages: 1. The locally best particles act as "scouts"
+in the sense that they move semi freely across search space and can approach different
+optima. The other particles will move to the best known position and thereby improving
+the swarms average cost.
+
+The optimization of the "scout-particles" involves Bayesian belief estimation of
+how likely the results of the MC simulation are. There are a number of parameters
+which enter the point update, which are dependent on the average win-rate of the
+to-be-optimized model. These can lead to large-ish steps even if the average win-rate
+lies is within the range that can be reasonably expected, given the number of
+samples. The posterior of the Bayesian belief estimation is than used to slow
+movement in search space until win-rates are re-evaluated next epoch or the particle
+is no longer locally the best.
+
+Such a belief estimation does not enter the optimization of the other particles.
+Hence, once a particle is no longer locally the best as well as close to a reasonably
+good point estimate, it will then move on the fastest route towards the best know
+position in search space.
+
+This has the following advantages: 1. "Scout particles" move freely but are less
+likely to overshoot optimal position. 2. Once an optimum has been found all particles
+will slowly converge towards it, switching between "scout" and normal roles.
+
+5. Discussion
+It needs to be repeated that this is a meta-heuristic. That is, a procedure which
+makes no assumption about the concrete problem itself but still has reasonable
+chances of finding a global optimum.
+
+Furthermore, as long as the optimal points of the individual models aren't perfect
+multiples of each other, one will never find a point where each model has a cost
+of 0. The best one can hope to find is the point where one cannot make a model
+better without making another model worse.
+
+Furthermore, the code uses a stochastic process to estimate the win rates. Even if
+the points are perfect, there will always be fluctuation on the order of 1/N^2,
+as we the mean squared distance to the optimum.
+
+These points suggest, that the final cost the code converges towards is, at best,
+the a metric for the best possible balance that can be found, plus stochastic
+fluctuations. One should never expect the code to converge perfectly towards 0.
+
+This is aggravated by trying to fit models which are vastly more powerful than the rest,
+as it will drastically undervalue the lower point costs.
+
+Additionally, this is likely not the best approach to balancing the game.
+A more sensible (but less cool) way would try to estimate the point value of each
+pip in a given characteristic, i.e. how valuable is improving armour by one point,
+etc... ?
+
+This could then be used to estimate initial point values for good old hands-on
+playtesting.
+
+Last but not least and apropos playtesting. The game of 40k is usually played
+over 5 rounds and for victory points. These are gained by achieving some objectives in
+the game. There is also a melee phase, model abilities, aura abilities, equipment,
+tanks, anti-infantry-keywords (which terribly skew stats)...
+
+So are the points estimated even remotely close to the actual value of a model
+in-game? No. However, this was a fun exercise in numerical optimization and I learned
+quite a lot about gradient-less optimization (as well as why the original idea was
+already flawed).
+
+Thank you for your time and I hope you had some fun playing with the code.
+
+
+
+
+
+
+#End
---
+++ warhammer_particle_swarm.py
@@ -0,0 +1,297 @@
+import numpy as np
+from scipy.stats import binom
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+class DataSheet():
+    def __init__(self,*,Name, Wounds, Toughness, Save, Invuln, Weapons):
+        self.name = Name
+        self.w = Wounds
+        self.sv = Save
+        self.inv = Invuln
+        self.t = Toughness
+        self.weapon = Weapons
+
+class Weapon():
+    def __init__(self,*, Attacks, Skill, Strength, Damage, ArmourPiercing):
+        self.at = Attacks
+        self.sk = Skill
+        self.st = Strength
+        self.d = Damage
+        self.ap = ArmourPiercing
+
+class unit():
+    def __init__(self, ID, datasheet, number):
+        self.id = ID
+        self.model = datasheet
+        self.number = number
+
+lasgun = Weapon(Attacks=2,Skill=4,Strength=3,Damage=1,ArmourPiercing=0)
+bolt_rifle = Weapon(Attacks=2,Skill=3,Strength=4,Damage=1,ArmourPiercing=1)
+heavy_bolt_rifle = Weapon(Attacks=2,Skill=3,Strength=5,Damage=2,ArmourPiercing=1)
+storm_bolter = Weapon(Attacks=4,Skill=3,Strength=4,Damage=1,ArmourPiercing=0)
+guardian_spear = Weapon(Attacks=2,Skill=2,Strength=4,Damage=2,ArmourPiercing=1)
+pulse_blaster = Weapon(Attacks=2,Skill=3,Strength=6,ArmourPiercing=1,Damage=1)
+
+guards_man = DataSheet(Name="Guard", Wounds=1,Toughness=3,Save=5,Invuln=7,Weapons=lasgun)
+tau_breacher = DataSheet(Name="Breacher",Wounds=1,Toughness=3,Save=4,Invuln=7,Weapons=pulse_blaster)
+intercessor = DataSheet(Name="Intercessor",Wounds=2,Toughness=4,Save=3,Invuln=7,Weapons=bolt_rifle)
+terminator = DataSheet(Name="Terminator",Wounds=3,Toughness=5,Save=2,Invuln=4,Weapons=storm_bolter)
+heavy_intercessor = DataSheet(Name="H. Intercessor",Wounds=3,Toughness=6,Save=3,Invuln=7,Weapons=heavy_bolt_rifle)
+custodian_guard = DataSheet(Name="Custodes",Wounds=3,Toughness=6,Save=2,Invuln=4,Weapons=guardian_spear)
+
+def RollD6(n):
+    return np.random.randint(6,size=n)+1
+
+def RollHits(model, count):
+    number_of_attacks = model.weapon.at*count
+    hit_rolls = RollD6(number_of_attacks)
+    number_of_successes = (hit_rolls>=model.weapon.sk).sum()
+    return number_of_successes
+
+def WoundRolls(strength,toughness,count):
+    ratio = toughness/strength
+    wound_rolls = RollD6(count)
+    if ratio >= 2:
+        number_of_successes = (wound_rolls>=6).sum()
+    elif ratio > 1:
+        number_of_successes = (wound_rolls>=5).sum()
+    elif ratio == 1:
+        number_of_successes = (wound_rolls>=4).sum()
+    elif ratio < 1 and ratio > 0.5:
+        number_of_successes = (wound_rolls>=3).sum()
+    else:
+        number_of_successes = (wound_rolls>=2).sum()
+    return number_of_successes
+
+def FailedSaveRolls(save,Invuln,count):
+    if save > Invuln:
+        return (RollD6(count)<Invuln).sum()
+    else:
+        return (RollD6(count)<save).sum()
+
+def HandleDamage(wounds,failed_saves,damage):
+    number_of_dead_models = 0
+    for attacks in range(failed_saves):
+        if damage >= wounds:
+            number_of_dead_models += 1
+        else:
+            number_of_dead_models += damage / wounds
+    return number_of_dead_models
+
+def CombatRound(attacker, defender):
+    '''
+    attacker: unit    
+    defender: unit    
+    
+    all models shoot, damage gets resolved, unit gets reduced
+    if number of deadmoedels > unit size => end combat with victory'''
+    number_of_hits = RollHits(attacker.model,np.ceil(attacker.number).astype(np.int32))
+    number_of_wounds = WoundRolls(attacker.model.weapon.st,defender.model.t,number_of_hits)
+    number_of_failed_saves = FailedSaveRolls(defender.model.sv + attacker.model.weapon.ap, defender.model.inv, number_of_wounds)
+    number_of_dead_models = HandleDamage(defender.model.w,number_of_failed_saves,attacker.model.weapon.d)
+    
+    defender.number -= number_of_dead_models
+    
+    return attacker, defender
+
+def SimulateRangedCombat(number_of_combats, model1, model2, points):
+    lcm = np.lcm(points[0],points[1])
+    wins = np.zeros(2) # unit1, unit2
+
+    unit1 = unit(0,model1,(lcm//points[0]).astype(np.float32))
+    unit2 = unit(1,model2,(lcm//points[1]).astype(np.float32))
+
+    for i in range(number_of_combats):        
+        unit1.number = (lcm//points[0]).astype(np.float32)      # initialise number of models 
+        unit2.number = (lcm//[points[1]]).astype(np.float32)
+
+        roll_off = np.random.randint(2) # decide intial attacker/defender of this fight
+
+        if roll_off == 0:
+            attacker = unit1
+            defender = unit2
+        else:
+            attacker = unit2
+            defender = unit1
+
+        while True:
+            attacker, defender = CombatRound(attacker, defender)
+            if defender.number <= 0 :
+                wins[attacker.id] += 1
+                break
+            else:
+                temp = attacker         # if the defender hasn't been destroyed, swap sides and go again
+                attacker = defender
+                defender = temp
+    return wins
+
+def DegreeOfMistrust(k,n,p):
+    # Function which is 0 at optimal point cost and maximum at least-optimal point costs
+    # Sums to 1 between 0 and n -> Probability distribuiton of 0-Hypothesis: "The point cost estimates are wrong."
+    return (k-p*n)**4 / (0.2 * ( n**5 * p**5 + ( n-n*p )**5))
+
+def DegreeOfTrust(data,p):
+    N = sum(data)
+    prior = 0.5
+    temp1 = prior * binom.pmf(data[0],N,p)
+    temp2 = prior * DegreeOfMistrust(data[0],N,p)
+    norm = temp1 + temp2
+    return temp1/norm, temp2/norm
+
+def BurnIn(burn_in_samples, number_of_particles, queue, points, cost, total_cost):
+    for particle in range(number_of_particles):
+        for i in range(len(queue)):
+            for j in range(i+1,len(queue)):
+                wins = SimulateRangedCombat(burn_in_samples,queue[i],queue[j],[points[particle,i],points[particle,j]])
+                cost[particle,0,i,j] = np.abs(0.5 - wins[0]/burn_in_samples)**2
+                cost[particle,0,i,-1] += wins[0]/burn_in_samples - 0.5
+                cost[particle,0,i,-2] += cost[particle,0,i,j]
+
+                cost[particle,1,i,j] =  wins[0]/burn_in_samples - 0.5
+                cost[particle,1,j,i] = -cost[particle,1,i,j]
+
+                cost[particle,0,j,i] = cost[particle,0,i,j]
+                cost[particle,0,j,-1] += cost[particle,1,j,i]
+                cost[particle,0,j,-2] += cost[particle,0,j,i]
+
+        print(f"BurnIn: {100*(particle+1)/number_of_particles:.4} %",end="\r" )
+        total_cost[particle] = (np.mean(cost[particle,0,:,-2]))
+
+    return cost, total_cost
+
+def OptimizationStep(nparticle, index,  number_of_samples, points, queue, cost):
+    new_cost = np.zeros((2,len(queue)+2))
+
+    for i in range(len(queue)):
+        if index == i:
+            new_cost[0,i] = 0
+        else:
+            wins = SimulateRangedCombat(number_of_samples,queue[index],queue[i],[points[nparticle,index], points[nparticle,i]])
+            new_cost[0,i] = np.abs(wins[0]/number_of_samples - 0.5)**2
+            new_cost[0,-1] += wins[0]/number_of_samples - 0.5
+            new_cost[0,-2] += np.abs(wins[0]/number_of_samples - 0.5)**2
+            new_cost[1,i] = wins[0]/number_of_samples - 0.5
+
+    cost[nparticle,0,index,:] = new_cost[0,:]
+    cost[nparticle,1,index,:] = new_cost[1,:]
+
+    cost[nparticle,0,:,index] = new_cost[0,0:len(queue)]
+    cost[nparticle,1,:,index] = -new_cost[1,0:len(queue)]
+
+    for i in range(len(queue)):
+        cost[nparticle,0,i,-1] = sum(cost[nparticle,1,i,0:len(queue)])
+        cost[nparticle,0,i,-2] = sum(cost[nparticle,0,i,0:len(queue)])
+    
+    return cost
+
+def Epoch(number_of_samples, number_of_particles, queue, points, cost, total_cost, best):
+    for particle in range(number_of_particles):
+        if total_cost[particle] <= total_cost[(particle - 1) % number_of_particles] and total_cost[particle] <= total_cost[(particle + 1) % number_of_particles]:   # if particle is locally the best, optimize by decreasing the largest contribution to the cost
+            ind = np.argmax(cost[particle,0,:,-2])  # pick largest contributor to cost
+            dir = cost[particle,0,ind,-1]           # optimize in direction that minimizes it
+
+            beta = 0.25  # learning rate
+            trust = DegreeOfTrust([(sum(cost[particle,1,0,0:len(queue)] + 0.5)*number_of_samples), len(queue)*number_of_samples - (sum(cost[particle,1,0,0:len(queue)] + 0.5)*number_of_samples)],0.5)[0] ## arrest the updates if we belief points are ok
+            update = max(((points[particle,ind]*(1 + dir  * beta * (1-trust) ) + np.sign(dir)*(1 - trust) )  ).astype(np.int32),1)
+            points[particle,ind] = update
+
+            cost = OptimizationStep(particle, ind,  number_of_samples, points, queue, cost)
+
+            total_cost[particle] = (np.mean(cost[particle,0,:,-2]))
+
+        else:   # if particle is not local best, progress towards best known position
+            dif = best - points[particle]   # direction to best known position
+            ind = np.argmax(np.abs(dif))    # axis of largest difference
+            
+            phi = 0.75  # learning rate
+            update = points[particle,ind] + np.round(phi*dif[ind]).astype(np.int32)
+            points[particle,ind] = update
+    
+            cost = OptimizationStep(particle, ind,  number_of_samples, points, queue, cost)
+
+            total_cost[particle] = (np.mean(cost[particle,0,:,-2]))
+
+    return cost, total_cost
+
+def Driver(number_of_epochs,        # Number of optimization steps
+           number_burnin_samples,   # Number of samples used during the intial setup
+           number_of_samples,       # number of samples used during optimization
+           number_of_particles,     # number of particles in the swarm
+           queue,                   # list of datasheets
+           lims                     # upper and lower bound of random point initialisation
+           ):
+    
+    points = np.random.randint(low=lims[0],high=lims[1],size=(number_of_particles,len(queue)))
+    cost = np.zeros((number_of_particles,2,len(queue),len(queue)+2)) 
+    total_cost = np.zeros(number_of_particles)
+    
+    cost_history = np.zeros((number_of_epochs+1,2))
+    point_history = np.zeros((number_of_epochs+1,len(queue)))
+
+    cost, total_cost = BurnIn(number_burnin_samples,number_of_particles,queue,points,cost,total_cost)
+
+    best = points[np.argmin(total_cost)]
+    cost_history[0,0] = np.min(total_cost)
+    cost_history[0,1] = np.mean(total_cost)
+    point_history[0] = best
+    
+    for ep in range(number_of_epochs):
+        cost, total_cost = Epoch(number_of_samples, number_of_particles, queue, points, cost, total_cost, best)
+    
+        best = points[np.argmin(total_cost)]
+    
+        cost_history[ep+1,0] = np.min(total_cost)
+        cost_history[ep+1,1] = np.mean(total_cost)
+        point_history[ep+1] = best
+        print(f"Progress: {100*(ep+1)/number_of_epochs:.4}%, ",f"Min: {cost_history[ep+1,0]:.4}, Mean: {cost_history[ep+1,1]:.4}  ", end="\r")
+    
+    ## Final Pass to get most accurate readings/
+    # cost, total_cost = BurnIn(2000,number_of_particles,queue,points,cost,total_cost)
+
+    return cost, total_cost, point_history, cost_history
+
+## ---------------- Execution ---------------- ##
+
+blo = 40 # limits for random point initialisation
+bhi = 60 # large spread decreases learning speed
+
+queue = [guards_man, intercessor, terminator, heavy_intercessor, custodian_guard] # 
+
+cost, total_cost, point_history, cost_history = Driver(number_of_epochs=500,number_burnin_samples=100, number_of_samples=20, number_of_particles=20, queue=queue, lims=[blo,bhi])
+
+c = cost[np.argmin(total_cost),1,:,0:len(queue)]
+## ---------------- Plotting ---------------- ##
+
+axticklabels = [model.name for model in queue]
+axticklabels.insert(0,"")
+
+plt.figure(figsize=(18,9))
+ax1 = plt.subplot(2,1,2)
+ax2 = plt.subplot(2,2,1)
+ax3 = plt.subplot(2,2,2)
+
+for i in range(len(queue)):
+    ax1.plot(point_history[:,i],label=axticklabels[i+1])
+ax1.grid()
+ax1.legend()
+ax1.set_xlabel("Epoch")
+ax1.set_ylabel("Points")
+
+ax2.plot(cost_history[:,0],label="Min(Cost)")
+ax2.plot(cost_history[:,1],label="Mean(Cost)")
+ax2.grid()
+ax2.legend()
+ax2.set_xlabel("Epoch")
+ax2.set_ylabel("Cost")
+
+im = ax3.imshow(c,clim=(-0.5,0.5),cmap="seismic")
+divider = make_axes_locatable(ax3)
+cax = divider.append_axes("right", size="5%", pad=0.05)
+plt.colorbar(im, cax=cax, orientation='vertical')
+ax3.set_xticklabels(axticklabels,rotation=25)
+ax3.set_yticklabels(axticklabels)
+
+plt.show()
+
+# End
